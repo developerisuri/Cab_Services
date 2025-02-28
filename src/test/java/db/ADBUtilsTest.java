f21c15be328c4c757004788f40b6039f9744526b
iband <iband@DESKTOP-840HA6F>
2025-02-28 14:29:54 +0530

    ADBUtils test runs

---
+++ src/test/java/db/ADBUtilsTest.java
@@ -0,0 +1,345 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package db;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author iband
+ */
+public class ADBUtilsTest {
+    
+    
+    public ADBUtilsTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getUseri method, of class ADBUtils.
+     */
+    
+    
+    // @Test
+   // public void testGetUser() throws Exception {
+        
+         //User expResult = new User(6,"Carlos Sainz", "6554");
+        //ADBUtils instance = new ADBUtils();
+        
+          //Test the reading
+       // User result = instance.getUseri(expResult.getId());
+      // assertEquals(expResult.getId(), result.getId());
+      // assertEquals(expResult.getUsername(), result.getUsername());
+      // assertEquals(expResult.getPassword(), result.getPassword());
+
+        
+        
+       //  Test the updating
+      // expResult.setUsername("Lando Norris");
+      //  boolean updated = instance.updateUser(expResult);
+     //  assertEquals(updated, true);
+      
+      
+        
+   // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   // @Test
+    //public void testGetUseri() throws Exception {
+      //  System.out.println("getUseri");
+       // int id = 0;
+       // ADBUtils instance = new ADBUtils();
+       // User expResult = null;
+        //User result = instance.getUseri(id);
+        //assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+   // }
+
+    /**
+     * Test of getUser method, of class ADBUtils.
+     */
+   // @Test
+   // public void testGetUser() {
+      //  System.out.println("getUser");
+      //  ADBUtils instance = new ADBUtils();
+      //  List<User> expResult = null;
+      //  List<User> result = instance.getUser();
+      //  assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of addUser method, of class ADBUtils.
+     */
+  //  @Test
+  //  public void testAddUser() {
+     //   System.out.println("addUser");
+    //    User us = null;
+     //   ADBUtils instance = new ADBUtils();
+      //  boolean expResult = false;
+      //  boolean result = instance.addUser(us);
+      //  assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      //  fail("The test case is a prototype.");
+   // }
+
+    /**
+     * Test of updateUser method, of class ADBUtils.
+     */
+  //  @Test
+  //  public void testUpdateUser() {
+   //     System.out.println("updateUser");
+    //    User us = null;
+    //    ADBUtils instance = new ADBUtils();
+    //    boolean expResult = false;
+    //    boolean result = instance.updateUser(us);
+     //   assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      //  fail("The test case is a prototype.");
+   // }
+
+    /**
+     * Test of deleteUser method, of class ADBUtils.
+     */
+  //  @Test
+  //  public void testDeleteUser() {
+   //     System.out.println("deleteUser");
+   //     int id = 0;
+   //     ADBUtils instance = new ADBUtils();
+   //     boolean expResult = false;
+    //    boolean result = instance.deleteUser(id);
+    //    assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+     //   fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of getAllVehicles method, of class ADBUtils.
+     */
+   // @Test
+   // public void testGetAllVehicles() {
+   //     System.out.println("getAllVehicles");
+    //    ADBUtils instance = new ADBUtils();
+    //    List<Vehicle> expResult = null;
+    //    List<Vehicle> result = instance.getAllVehicles();
+     //   assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      //  fail("The test case is a prototype.");
+   // }
+
+    /**
+     * Test of getVehicleById method, of class ADBUtils.
+     */
+   // @Test
+  //  public void testGetVehicleById() throws Exception {
+   //     System.out.println("getVehicleById");
+   //     int id = 0;
+   //     ADBUtils instance = new ADBUtils();
+    //    Vehicle expResult = null;
+   //     Vehicle result = instance.getVehicleById(id);
+   //     assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+     //   fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of addVehicle method, of class ADBUtils.
+     */
+  //  @Test
+  //  public void testAddVehicle() {
+   //     System.out.println("addVehicle");
+   //     Vehicle vehicle = null;
+   //     ADBUtils instance = new ADBUtils();
+   //     boolean expResult = false;
+   //     boolean result = instance.addVehicle(vehicle);
+    //    assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+     //   fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of updateVehicle method, of class ADBUtils.
+     */
+   // @Test
+  //  public void testUpdateVehicle() {
+   //     System.out.println("updateVehicle");
+   //     Vehicle vehicle = null;
+    //    ADBUtils instance = new ADBUtils();
+    //    boolean expResult = false;
+    //    boolean result = instance.updateVehicle(vehicle);
+     //   assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      //  fail("The test case is a prototype.");
+   // }
+
+    /**
+     * Test of deleteVehicle method, of class ADBUtils.
+     */
+   // @Test
+ //   public void testDeleteVehicle() {
+  //      System.out.println("deleteVehicle");
+   //     int vehicleId = 0;
+   //     ADBUtils instance = new ADBUtils();
+   //     boolean expResult = false;
+    //    boolean result = instance.deleteVehicle(vehicleId);
+    //    assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+     //   fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of getAllDrivers method, of class ADBUtils.
+     */
+   // @Test
+  //  public void testGetAllDrivers() {
+  //      System.out.println("getAllDrivers");
+   //     ADBUtils instance = new ADBUtils();
+   //     List<Driver> expResult = null;
+   //     List<Driver> result = instance.getAllDrivers();
+  //      assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    //    fail("The test case is a prototype.");
+   // }
+
+    /**
+     * Test of getDriverById method, of class ADBUtils.
+     */
+  //  @Test
+ //   public void testGetDriverById() throws Exception {
+   //     System.out.println("getDriverById");
+   //     int id = 0;
+    //    ADBUtils instance = new ADBUtils();
+    //    Driver expResult = null;
+    //    Driver result = instance.getDriverById(id);
+      //  assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+     //   fail("The test case is a prototype.");
+   // }
+
+    /**
+     * Test of addDriver method, of class ADBUtils.
+     */
+    //@Test
+   // public void testAddDriver() {
+    //    System.out.println("addDriver");
+    //    Driver driver = null;
+    //    ADBUtils instance = new ADBUtils();
+    //    boolean expResult = false;
+     //   boolean result = instance.addDriver(driver);
+     //   assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      //  fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of updateDriver method, of class ADBUtils.
+     */
+  //  @Test
+  //  public void testUpdateDriver() {
+   //     System.out.println("updateDriver");
+    //    Driver driver = null;
+    //    ADBUtils instance = new ADBUtils();
+    //    boolean expResult = false;
+    //    boolean result = instance.updateDriver(driver);
+    //    assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+     //   fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of getAllHelpInstructions method, of class ADBUtils.
+     */
+   // @Test
+   // public void testGetAllHelpInstructions() {
+   //     System.out.println("getAllHelpInstructions");
+   //     ADBUtils instance = new ADBUtils();
+   //     List<help> expResult = null;
+   //     List<help> result = instance.getAllHelpInstructions();
+   //     assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    //    fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of getHelpById method, of class ADBUtils.
+     */
+  //  @Test
+  //  public void testGetHelpById() throws Exception {
+  //     System.out.println("getHelpById");
+   //     int id = 0;
+   //     ADBUtils instance = new ADBUtils();
+   //     help expResult = null;
+   //     help result = instance.getHelpById(id);
+   //     assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+   //     fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of addHelp method, of class ADBUtils.
+     */
+  //  @Test
+  //  public void testAddHelp() {
+   //     System.out.println("addHelp");
+   //     help help = null;
+   //     ADBUtils instance = new ADBUtils();
+    //    boolean expResult = false;
+    //    boolean result = instance.addHelp(help);
+    //    assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    //    fail("The test case is a prototype.");
+  //  }
+
+    /**
+     * Test of updateHelp method, of class ADBUtils.
+     */
+  //  @Test
+  //  public void testUpdateHelp() {
+  //      System.out.println("updateHelp");
+   //     help help = null;
+   //     ADBUtils instance = new ADBUtils();
+   //     boolean expResult = false;
+   //     boolean result = instance.updateHelp(help);
+   //     assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    //    fail("The test case is a prototype.");
+  //  }
+    
+}
